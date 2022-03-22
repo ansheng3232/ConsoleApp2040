2be51e18dc76da481aef2955c9a05d8338695bfb
Chen An <v-chenan1@microsoft.com>
2022-03-22 09:08:04 +0000

    1

--- WebApplication4/WeatherForecast.cs
+++ WebApplication4/WeatherForecast.cs
@@ -3,7 +3,7 @@ namespace WebApplication4
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
+        //1
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
